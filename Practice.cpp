1f8f232c5def4c5f67d1e983f60bfb99eaae237b
S1nuso1d <137918474+S1nuso1d@users.noreply.github.com>
2024-06-27 14:50:13 +0300

    Update Practice.cpp
    1.Cделал методы string getData() виртуальными, а оба класса наследуются от общего класса-интерфейса, который определяет субъект с единственным нулевым виртуальным методом getData()
    2.Добавил еще одного заместителя, который позволяет получить данные супер-пользователя. Помимо логина и пароля он должен запрашивать ключ доступа. Возвращать он должен секретные данные с добавленной к ним информацией об их владельце. Вроде такого: "Секретные данные (Иван Иванов)".

--- Practice.cpp
+++ Practice.cpp
@@ -1,23 +1,32 @@
-﻿#include <iostream>
-#include <string> 
+#include <iostream>
+#include <string>
 #include <clocale>
 #include <conio.h>
 
 using namespace std;
 
-class SecureData
+// Интерфейс для субъекта
+class SecureDataSubject 
 {
 public:
-    string getData()
+    virtual string getData() = 0;
+};
+
+// Реальный объект с секретными данными
+class SecureData : public SecureDataSubject 
+{
+public:
+    string getData() override 
     {
         return "Секретные данные";
     }
 };
 
-class SecureDataProxy
+// Заместитель для доступа к секретным данным
+class SecureDataProxy : public SecureDataSubject 
 {
 private:
-    SecureData* realData;
+    SecureDataSubject* realData;
     string username;
     string password;
 
@@ -25,26 +34,65 @@ public:
     SecureDataProxy(const string& username, const string& password) :
         realData(nullptr), username(username), password(password) {}
 
-    string getData()
+    string getData() override 
     {
-        if (authenticate(username, password))
+        if (authenticate(username, password)) 
         {
-            if (realData == nullptr)
+            if (realData == nullptr) 
             {
                 realData = new SecureData();
             }
             return realData->getData();
         }
-        else
+        else 
         {
             return "Доступ запрещен.";
         }
     }
 
 private:
-    bool authenticate(const string& username, const string& password)
+    bool authenticate(const string& username, const string& password) 
     {
-        if (username == "user" && password == "password")
+        if (username == "user" && password == "password") 
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+// Заместитель для супер-пользователя
+class SuperUserProxy : public SecureDataSubject 
+{
+private:
+    SecureDataSubject* realData;
+    string username;
+    string password;
+    string accessKey;
+
+public:
+    SuperUserProxy(const string& username, const string& password, const string& accessKey) :
+        realData(nullptr), username(username), password(password), accessKey(accessKey) {}
+
+    string getData() override 
+    {
+        if (authenticate(username, password, accessKey)) 
+        {
+            if (realData == nullptr) 
+            {
+                realData = new SecureData();
+            }
+            return realData->getData() + " (Иван Иванов)"; // Добавлен владелец данных
+        }
+        else {
+            return "Доступ запрещен.";
+        }
+    }
+
+private:
+    bool authenticate(const string& username, const string& password, const string& accessKey) 
+    {
+        if (username == "superuser" && password == "superpassword" && accessKey == "secretkey") 
         {
             return true;
         }
@@ -57,27 +105,30 @@ void clearScreen()
     system("cls");
 }
 
-int main()
+int main() 
 {
     setlocale(LC_ALL, "Ru");
 
     string User;
     string Password;
+    string AccessKey;
 
     while (true) 
     {
         clearScreen();
         cout << "\t\t  *** Секретные данные ***\n";
-        cout << "\t\t___________________________\n";
-        cout << "\t\t| 1. Вход                 |\n";
-        cout << "\t\t| 2. Выход                |\n";
-        cout << "\t\t---------------------------\n";
+        cout << "\t\t______________________________\n";
+        cout << "\t\t| 1. Вход                    |\n";
+        cout << "\t\t| 2. Вход супер-пользователя |\n";
+        cout << "\t\t| 3. Выход                   |\n";
+        cout << "\t\t------------------------------\n";
         cout << "\t\t| Выберите действие: ";
 
         int choice;
         cin >> choice;
 
-        if (choice == 1) {
+        if (choice == 1) 
+        {
             clearScreen();
             cout << "\t\t\t      *** Вход ***\n";
             cout << "\t\t\t_______________________\n";
@@ -93,11 +144,33 @@ int main()
             cout << "\t\t\tНажмите любую клавишу, чтобы вернуться в меню...";
             _getch();
         }
-        else if (choice == 2) {
+        else if (choice == 2) 
+        {
+            clearScreen();
+            cout << "\t\t\t      *** Вход супер-пользователя ***\n";
+            cout << "\t\t\t___________________________________________\n";
+            cout << "\t\t\t|User:     | ";
+            cin >> User;
+            cout << "\t\t\t-------------------------------------------\n";
+            cout << "\t\t\t|Password: | ";
+            cin >> Password;
+            cout << "\t\t\t-------------------------------------------\n";
+            cout << "\t\t\t|Access Key:| ";
+            cin >> AccessKey;
+            cout << "\t\t\t-------------------------------------------\n";
+
+            SuperUserProxy superUserProxy(User, Password, AccessKey);
+            cout << "\t\t\t" << superUserProxy.getData() << endl;
+            cout << "\t\t\tНажмите любую клавишу, чтобы вернуться в меню...";
+            _getch();
+        }
+        else if (choice == 3) 
+        {
             cout << "\t\t\tДо свидания!\n";
             break;
         }
-        else {
+        else 
+        {
             cout << "\t\t\tНеверный выбор. Попробуйте снова.\n";
             _getch();
         }
